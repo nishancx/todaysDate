6ba6df99af692e7d58179215e2dcd1b43612ea1e
Subash Pathak <Subash@users.noreply.github.com>
2020-05-12 15:50:25 +0545

    reload pwa

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 // the person who removes this sucks their own toes
 const path = require('path');
 const express = require('express');
-const {
-  toBS
-} = require('@sbspk/bs');
+const { toBS } = require('@sbspk/bs');
 
 const bsMonths = [
   "Baishakh", "Jestha", "Ashadh", "Shrawan", "Bhadra", "Ashwin",
@@ -18,37 +16,21 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
   // get details in NPT
   const format = (options) => {
-    return new Intl.DateTimeFormat('en-US', {
-      timeZone: 'Asia/Kathmandu',
-      ...options
-    }).format(new Date());
+    return new Intl.DateTimeFormat('en-US', { timeZone: 'Asia/Kathmandu', ...options }).format(new Date());
   }
 
   const adDate = {
-    year: Number.parseInt(format({
-      year: 'numeric'
-    }), 10),
-    month: Number.parseInt(format({
-      month: 'numeric'
-    }), 10),
-    day: Number.parseInt(format({
-      day: 'numeric'
-    }), 10),
-    monthName: format({
-      month: 'long'
-    }),
-    dayName: format({
-      weekday: 'short'
-    }),
+    year: Number.parseInt(format({ year: 'numeric' }), 10),
+    month: Number.parseInt(format({ month: 'numeric' }), 10),
+    day: Number.parseInt(format({ day: 'numeric' }), 10),
+    monthName: format({ month: 'long' }),
+    dayName: format({ weekday: 'short' }),
   };
 
   const bsDate = toBS(adDate);
-  bsDate.monthName = bsMonths[bsDate.month - 1],
+  bsDate.monthName = bsMonths[bsDate.month - 1];
 
-    res.render('index', {
-      bsDate,
-      adDate
-    });
+  res.render('index', { bsDate, adDate });
 });
 
 app.listen(8000);

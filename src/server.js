a90de2ad1813bb1fe5c2328c555c0588d706baad
nishan <nishan@users.noreply.github.com>
2020-05-12 14:51:54 +0545

    Removed public/static folder

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 // the person who removes this sucks their own toes
 const path = require('path');
 const express = require('express');
-const { toBS } = require('@sbspk/bs');
+const {
+  toBS
+} = require('@sbspk/bs');
 
 const bsMonths = [
   "Baishakh", "Jestha", "Ashadh", "Shrawan", "Bhadra", "Ashwin",
@@ -15,22 +17,38 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   // get details in NPT
-  const format = (options)=> {
-    return new Intl.DateTimeFormat('en-US', { timeZone: 'Asia/Kathmandu', ...options }).format(new Date());
+  const format = (options) => {
+    return new Intl.DateTimeFormat('en-US', {
+      timeZone: 'Asia/Kathmandu',
+      ...options
+    }).format(new Date());
   }
 
   const adDate = {
-    year: Number.parseInt(format({ year: 'numeric'}), 10),
-    month: Number.parseInt(format({ month: 'numeric'}), 10),
-    day: Number.parseInt(format({ day: 'numeric'}), 10),
-    monthName: format({ month: 'long'}),
-    dayName: format({ weekday: 'short'}),
+    year: Number.parseInt(format({
+      year: 'numeric'
+    }), 10),
+    month: Number.parseInt(format({
+      month: 'numeric'
+    }), 10),
+    day: Number.parseInt(format({
+      day: 'numeric'
+    }), 10),
+    monthName: format({
+      month: 'long'
+    }),
+    dayName: format({
+      weekday: 'short'
+    }),
   };
 
   const bsDate = toBS(adDate);
   bsDate.monthName = bsMonths[bsDate.month - 1],
 
-  res.render('index', { bsDate, adDate });
+    res.render('index', {
+      bsDate,
+      adDate
+    });
 });
 
 app.listen(8000);

d48e4cef2dabef87b8c8257df65ca1cb47fc2180
Subash Pathak <Subash@users.noreply.github.com>
2020-05-12 20:30:35 +0545

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,28 +3,31 @@ const path = require('path');
 const express = require('express');
 const { toBS } = require('@sbspk/bs');
 
-const bsMonths = [
-  "Baishakh", "Jestha", "Ashadh", "Shrawan", "Bhadra", "Ashwin",
-  "Kartik", "Mangsir", "Poush", "Magh", "Falgun", "Chaitra"
-];
-
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-  // get details in NPT
-  const format = (options) => {
-    return new Intl.DateTimeFormat('en-US', { timeZone: 'Asia/Kathmandu', ...options }).format(new Date());
-  }
+const bsMonths = [
+  "Baishakh", "Jestha", "Ashadh", "Shrawan", "Bhadra", "Ashwin",
+  "Kartik", "Mangsir", "Poush", "Magh", "Falgun", "Chaitra"
+];
 
+// get formatted date in NPT
+const date = (options) => {
+  return new Intl.DateTimeFormat('en-US', {
+    timeZone: 'Asia/Kathmandu',
+    ...options
+  }).format(new Date());
+}
+
+app.get('/', (req, res) => {
   const adDate = {
-    year: Number.parseInt(format({ year: 'numeric' }), 10),
-    month: Number.parseInt(format({ month: 'numeric' }), 10),
-    day: Number.parseInt(format({ day: 'numeric' }), 10),
-    monthName: format({ month: 'long' }),
-    dayName: format({ weekday: 'short' }),
+    year: Number.parseInt(date({ year: 'numeric' }), 10),
+    month: Number.parseInt(date({ month: 'numeric' }), 10),
+    day: Number.parseInt(date({ day: 'numeric' }), 10),
+    monthName: date({ month: 'long' }),
+    dayName: date({ weekday: 'short' }),
   };
 
   const bsDate = toBS(adDate);
